6098359d5a4d979bc41009a1e54059af6424864b
Harut101 <arutaydinyan@gmail.com>
2019-06-09 15:00:54 +0400

    refactor chart data generator function

--- src/helpers/chartDataGenerator.js
+++ src/helpers/chartDataGenerator.js
@@ -1,15 +1,20 @@
 
-export function dateGenerator(date){
-    let dateInterval = [];
+export function chartDataGenerator(date , data){
+    let chartData = [];
     let nextDate =  date;
-    for(let i = 0; i < 7; ++i ){
+    let amt = 1060;
+    
+    for(let i = 0; i < data.length; ++i ){
     
        let nextDateFormat = `${new Date(nextDate).getDate()}.${new Date(nextDate).getMonth() + 1}.${new Date(nextDate).getFullYear()}`;
+
+       chartData.push({name: nextDateFormat, uv: data[i][1], amt: amt});
+
+       amt -= 100;
        
        nextDate.setDate(nextDate.getDate() + 1);
 
-       dateInterval.push(nextDateFormat);
     }
 
-   return dateInterval;
+   return chartData;
 }
